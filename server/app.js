734b2d5876427d7fb3810fe2841c14f7ebd2ef93
WallMold <159772899+WallMold@users.noreply.github.com>
2025-04-01 02:02:43 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
- const path = require('path');
- 
- const app = express();
- const PORT = 1337;
- 
- app.use(express.static(__dirname)); 
- 
- app.get('/page1', (req, res) => {
-     res.sendFile(path.join(__dirname, 'page1.html'));
+const path = require('path');
 
-  app.get('/login', (req, res) => {
-     res.sendFile(path.join(__dirname, 'login.html'));
- });
- 
- app.listen(PORT, () => {
-     console.log(`Server running at http://localhost:${PORT}/`);
- });
+const app = express();
+const PORT = 1337;
+
+app.use(express.static(__dirname));
+
+// Route for page1.html
+app.get('/page1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'page1.html'));
+});
+
+// Route for login.html
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
