61e98612f37eaefe02338bed7a57e48face69373
WallMold <159772899+WallMold@users.noreply.github.com>
2025-04-25 01:03:38 -0400

    Update userRoute.js

--- server/route/userRoute.js
+++ server/route/userRoute.js
@@ -1,31 +1,21 @@
-var userController = require('../service/userService');
-// require the express library
-var express = require('express');
-// create chainable route handlers for a path by using app.route()
+const express = require('express');
+const userController = require('../controller/userController');
+const router = express.Router();
 
-
-// see https://expressjs.com/en/guide/routing.html
-var router = express.Router();
-
-// all users routes
+// all users routes (GET all users, POST new user)
 router.route('/')
-.get( ( req, res ) => {
-userController.getUsers( req, res );
-})
-.post( ( req, res ) => {
-userController.saveUser( req, res );
-}
-);
+  .get((req, res) => {
+    userController.getUsers(req, res);
+  })
+  .post((req, res) => {
+    userController.saveUser(req, res);
+  });
 
-// user by array index route
-router.route('/:UserId')
-.get( ( req, res ) => {
-userController.getUser( req, res );
-}
-);
+// user by index route (GET one user)
+router.route('/:index')
+  .get((req, res) => {
+    userController.getUser(req, res);
+  });
 
-
-// output to the log that the module is loaded successfully!
 console.log("[userRoute] initialized");
-
 module.exports = router;
