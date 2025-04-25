a84ad9f335fcd55fc23ba92dd8b6ee73e454905f
WallMold <159772899+WallMold@users.noreply.github.com>
2025-04-25 01:02:58 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = 1337;
 
+// parse JSON bodies
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname));
 
 // Route for page1.html
 app.get('/page1', (req, res) => {
-    res.sendFile(path.join(__dirname, 'page1.html'));
+  res.sendFile(path.join(__dirname, 'page1.html'));
 });
 
 // Route for login.html
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'login.html'));
+  res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
 
 // Include the userRoutes module
-let userRoute = require('./route/userRoute');
+const userRoute = require('./route/userRoute');
 app.use('/api/user', userRoute);
